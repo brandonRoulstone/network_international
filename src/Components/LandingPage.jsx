c11d5bfcc4ec08cba3f6b2c22db7488aad45ace5
Brandon <brandonroulstone71@gmail.com>
2024-06-25 15:13:34 +0200

    last commit

--- src/Components/LandingPage.jsx
+++ src/Components/LandingPage.jsx
@@ -6,6 +6,7 @@ const LandingPage = () => {
       <div>
         <PaymentsPage />
       </div>
+      
       <div className=" my-5 bg-tertiary py-5 shadow-lg flex justify-content-center row container-fluid">
         <p className="text-black fw-bold">Click the button below to access the payment gateway</p>
         <GetToken />
